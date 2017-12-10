18e329eda3d238e97b2c8ea561f5dd8d4bcb460c
Gurprit Mukhraiya <gurupritmukhraiya@gmail.com>
2017-12-10 13:09:58 +0530

    First Commit

---
+++ flycap10/src/main/java/com/flycap10/Flycap10Application.java
@@ -0,0 +1,12 @@
+package com.flycap10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flycap10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flycap10Application.class, args);
+	}
+}
